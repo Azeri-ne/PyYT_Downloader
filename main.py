dfa94754822f731460fde61b48a8a7dc7debe046
Azerine <alterna811@gmail.com>
2024-11-24 11:03:44 +0800

    Add menu

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 import downloader as dl
 
 def main() -> None:
-    dl.get_titles_in_playlist(r'https://www.youtube.com/watch?v=EPKheS54XfQ&list=PLcv48kSSl5JmZfepEANfFX-0ZpNNkyZHh')
-    return
+    while True:
+        show_menu()
+
+        choice = get_choice()
+        if choice == '0':
+            break
+
+    print("Shutting down.")
+
+
+def show_menu() -> None:
+    print('''
+    ***~ PyYT Downloader ~***
+          
+    [0] Exit
+    [1] Download one video
+    [2] Download one video, audio only
+    [3] Download a playlist
+    [4] Download a playlist, audio only 
+          ''')
+
+
+def get_choice() -> str:
+    choice = input("What are we stealing today? ")
+
+    return choice
 
 
 if __name__ == '__main__':
