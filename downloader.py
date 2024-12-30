5e07820bf01e49df8ff535f0bf919c4c9b915e4a
Azerine <alterna811@gmail.com>
2024-12-31 06:46:02 +0800

    Added new get_stream() function

--- downloader.py
+++ downloader.py
@@ -5,27 +5,98 @@ CURRENT_DIRECTORY = Path(__file__).parent
 DOWNLOAD_PATH = CURRENT_DIRECTORY / 'Downloads'
 
 def main() -> None:
-    pass
+    download_single_video(r'https://www.youtube.com/watch?v=ld1GJ0zvsas')
+
+def handle_playlist_error(error, failed_downloads, 
+                          downloaded_videos, video) -> None:
+            print(f"Error! Can't find video. ({error})")
+            failed_downloads.append(video.title)
+            downloaded_videos += 1
+
+
+def list_failed_videos(failed_downloads) -> None:
+        print("These audio failed to download:")
+        for failed_download in failed_downloads:
+            print(failed_download)
+
+
+def print_border() -> None:
+    print("+--------------------+")
 
 
 def get_audio_stream(url:str) -> str:
-    yt = YouTube(url, 
-                 use_po_token=True)
+    yt = YouTube(url, use_po_token=True)
     print(yt.title)
 
     audio_stream = yt.streams.get_audio_only()
 
     return audio_stream
 
+def check_for_valid_resolution(resolution:int) -> bool:
+    print(resolution)
+    valid_resolutions = [144, 240, 360, 480, 720, 1080]
+
+    if resolution in valid_resolutions:
+        print("True")
+        return True
+
+    print("False")
+    return False
+
+# New get_stream function WIP
+def get_stream(url:str, stream_type:str, *resolution:int):
+    if not url.startswith('https://www.youtube.com'):
+        print("Error: Invalid URL")
+        # Log this
+        return None
+
+    if len(resolution) > 1:
+        print("Error: Only one resolution is allowed")
+        # You can pick the make it return 1 resolution with the highest instead
+        return None
+
+    video_resolution = resolution[0]
+    yt = YouTube(url, use_po_token=True)
+    print(yt.title)
+
+    if stream_type == 'audio':
+        audio_stream = yt.streams.get_audio_only()
+
+        return audio_stream
+
+    if stream_type == 'video':
+        if check_for_valid_resolution(video_resolution):
+            video_stream = yt.streams.get_by_resolution(video_resolution)
+            
+            return video_stream
+
+        print("Error: Invalid resolution")
+        return None
+
+    print("Error: Stream type is invalid")
+    return None
+
 
 def download_single_audio(url:str) -> None:
-    audio_stream = get_audio_stream(url)
+    audio_stream = get_stream(url, 'audio')
+
     try:
         audio_stream.download(output_path=DOWNLOAD_PATH)
 
     except Exception as error:
         print(f"Error! Download failed: ({error})")
-    print("+--------------------+")
+    print_border()
+
+
+def download_single_video(url:str, resolution:int) -> None:
+    video_stream = get_stream(url, 'video', resolution)
+
+    try:
+        video_stream.download(output_path=DOWNLOAD_PATH)
+
+    except Exception as error:
+        print(f"Error! Can't find video. ({error})")
+    print_border()
 
 
 def download_playlist_audio(url:str) -> None:
@@ -34,45 +105,29 @@ def download_playlist_audio(url:str) -> None:
 
     playlist_folder = DOWNLOAD_PATH / playlist_title
     playlist_folder.mkdir(parents=True, exist_ok=True)
-    downloaded_videos = 0
 
+    downloaded_videos = 0
     failed_downloads = []
 
     for video in playlist.videos:
         try:
-            audio_stream = get_audio_stream(url)
+            print(video.title)
+            audio_stream = video.streams.get_audio_only()
             audio_stream.download(output_path=playlist_folder,
-                               filename_prefix=f"{downloaded_videos + 1} - ",
-                               max_retries=3)
+                filename_prefix=f"{downloaded_videos + 1} - ",
+                max_retries=3)
             downloaded_videos += 1
 
         except Exception as error:
-            print(f"Error! Can't find video. ({error})")
-            failed_downloads.append(video.title)
-            downloaded_videos += 1
+            handle_playlist_error(error, failed_downloads,
+                                  downloaded_videos, video)
 
             continue
 
     print(f"Process complete: {downloaded_videos} audio downloaded.")
     if failed_downloads:
-        print("These audio failed to download:")
-        for failed_download in failed_downloads:
-            print(failed_download)
-    print("+--------------------+")
-
-
-def download_single_video(url:str) -> None:
-    yt = YouTube(url,
-                 use_po_token=True)
-    print(yt.title)
-
-    yt_stream = yt.streams.get_by_resolution(1440)
-    try:
-        yt_stream.download(output_path=DOWNLOAD_PATH)
-
-    except Exception as error:
-        print(f"Error! Can't find video. ({error})")
-    print("+--------------------+")
+        list_failed_videos(failed_downloads)
+    print_border()
 
 
 def download_playlist_video(url:str) -> None:
